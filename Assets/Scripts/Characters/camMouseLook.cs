fa5fd214832f3c5faa5c683811addf1db105b835
Gryphoss <darrengriffiths316@outlook.com>
2017-11-28 14:18:59 +0000

    Camera
    Add script to control the camera and lock to player

---
+++ Assets/Scripts/Characters/camMouseLook.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camMouseLook : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+
+	// Use this for initialization
+	void Start ()
+    {
+        character = this.transform.parent.gameObject;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale == 1)
+        {
+            var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+            md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+            smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing); //Move smoother between points by using lerp
+            smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing); //Move smoother between points
+            mouseLook += smoothV;
+
+            mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+            character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+
+            }
+        }
+    }
+}
