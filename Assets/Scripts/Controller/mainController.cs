66d8496ddc8cabdc076e7d24105f388d39efb62b
Gryphoss <darrengriffiths316@outlook.com>
2017-11-28 14:06:55 +0000

    mainController
    Add script

---
+++ Assets/Scripts/Controller/mainController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainController : MonoBehaviour {
+
+    public GameObject boltCutter;
+    public GameObject inventory;
+    int spawnNum = 1;
+
+    void spawn()
+    {
+        for (int i = 0; i < spawnNum; i++)
+        {
+            Vector3 boltCutterPos = new Vector3(this.transform.position.x + Random.Range(-1.0f, 1.0f),
+                                                this.transform.position.y + Random.Range(1.0f, 2.0f),
+                                                this.transform.position.z + Random.Range(-1.0f, 1.0f));
+            Instantiate(boltCutter, boltCutterPos, Quaternion.identity);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        spawn();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+
+        }
+	}
+}
